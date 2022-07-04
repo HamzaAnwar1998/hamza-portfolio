538779b0aa2fdbfcf300e380fd18985182468a99
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-07-04 21:14:00 +0500

    add default pdf layout

--- src/components/ResumeModal.js
+++ src/components/ResumeModal.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { Worker } from '@react-pdf-viewer/core';
 // Import the main component
 import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 // Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 export const ResumeModal = ({setResumeModal, resume}) => {
+
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  
   return (
     <div className='modal-background'>
         <div className='resume-modal-container'>
             <div className='delete-icon' onClick={()=>setResumeModal(false)}>x</div>
             {resume!==null&&(
-              // <embed src={resume} type="application/pdf" width={100+'%'} height={100+'%'}></embed>
               <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
-                <Viewer fileUrl={resume} />
+                <Viewer plugins={[defaultLayoutPluginInstance]} fileUrl={resume} />
               </Worker>
             )}
         </div>
